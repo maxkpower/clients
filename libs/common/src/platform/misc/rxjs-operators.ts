abb254cafba0c63c9558a4498973956050276160
Thomas Rittson <trittson@bitwarden.com>
2024-10-28 14:10:27 +1000

    Remove unnecessary type

--- libs/common/src/platform/misc/rxjs-operators.ts
+++ libs/common/src/platform/misc/rxjs-operators.ts
@@ -1,9 +1,7 @@
 import { map } from "rxjs";
 
-type ObjectWithId<TId> = { id: TId };
-
-export const getById = <TId, T extends ObjectWithId<TId>>(id: TId) =>
+export const getById = <TId, T extends { id: TId }>(id: TId) =>
   map<T[], T>((objects) => objects.find((o) => o.id === id));
 
-export const getByIds = <TId, T extends ObjectWithId<TId>>(ids: TId[]) =>
+export const getByIds = <TId, T extends { id: TId }>(ids: TId[]) =>
   map<T[], T[]>((objects) => objects.filter((o) => ids.includes(o.id)));
