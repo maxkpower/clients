d4c2bffbd5baa7ee4288d57b1e596a92175d6400
Thomas Rittson <trittson@bitwarden.com>
2024-10-28 14:43:25 +1000

    Fix typing

--- libs/common/src/platform/misc/rxjs-operators.ts
+++ libs/common/src/platform/misc/rxjs-operators.ts
@@ -1,7 +1,7 @@
 import { map } from "rxjs";
 
 export const getById = <TId, T extends { id: TId }>(id: TId) =>
-  map<T[], T>((objects) => objects.find((o) => o.id === id));
+  map<T[], T | undefined>((objects) => objects.find((o) => o.id === id));
 
 export const getByIds = <TId, T extends { id: TId }>(ids: TId[]) =>
   map<T[], T[]>((objects) => {
