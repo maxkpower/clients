9f8696f377dde48007a58a1d0de3f2205adad6e0
Jared Snider <jsnider@bitwarden.com>
2024-12-16 15:42:39 -0500

    PM-8113 - Move TwoFactorAuthEmail content to own folder.

--- libs/auth/src/angular/two-factor-auth/child-components/index.ts
+++ libs/auth/src/angular/two-factor-auth/child-components/index.ts
@@ -1,2 +1,1 @@
-export * from "./two-factor-auth-email-component.service";
-export * from "./default-two-factor-auth-email-component.service";
+export * from "./two-factor-auth-email";
--- libs/auth/src/angular/two-factor-auth/two-factor-auth.component.ts
+++ libs/auth/src/angular/two-factor-auth/two-factor-auth.component.ts
@@ -43,7 +43,7 @@ import {
 
 import { TwoFactorAuthAuthenticatorComponent } from "./child-components/two-factor-auth-authenticator.component";
 import { TwoFactorAuthDuoComponent } from "./child-components/two-factor-auth-duo.component";
-import { TwoFactorAuthEmailComponent } from "./child-components/two-factor-auth-email.component";
+import { TwoFactorAuthEmailComponent } from "./child-components/two-factor-auth-email/two-factor-auth-email.component";
 import { TwoFactorAuthWebAuthnComponent } from "./child-components/two-factor-auth-webauthn.component";
 import { TwoFactorAuthYubikeyComponent } from "./child-components/two-factor-auth-yubikey.component";
 import {
