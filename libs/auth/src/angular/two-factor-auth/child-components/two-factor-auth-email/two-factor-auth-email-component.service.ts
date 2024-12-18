1b660491ed1b5c4318816f8646231208b4c6dc41
Jared Snider <jsnider@bitwarden.com>
2024-12-18 17:19:39 -0500

    PM-8113 - TwoFactorAuthEmailComponentService - add docs

--- libs/auth/src/angular/two-factor-auth/child-components/two-factor-auth-email/two-factor-auth-email-component.service.ts
+++ libs/auth/src/angular/two-factor-auth/child-components/two-factor-auth-email/two-factor-auth-email-component.service.ts
@@ -1,3 +1,6 @@
+/**
+ * A service that manages all cross client functionality for the email 2FA component.
+ */
 export abstract class TwoFactorAuthEmailComponentService {
   /**
    * Optionally shows a warning to the user that they might need to popout the
