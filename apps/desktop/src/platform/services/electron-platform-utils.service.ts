22cc9f47c2c4e6e43e438080c7bac8f105aa8144
Bernd Schoolmann <mail@quexten.com>
2025-01-13 09:59:24 +0100

    Add support for fido2 2fa on mac

--- apps/desktop/src/platform/services/electron-platform-utils.service.ts
+++ apps/desktop/src/platform/services/electron-platform-utils.service.ts
@@ -80,7 +80,9 @@ export class ElectronPlatformUtilsService implements PlatformUtilsService {
   // Temporarily restricted to only Windows until https://github.com/electron/electron/pull/28349
   // has been merged and an updated electron build is available.
   supportsWebAuthn(win: Window): boolean {
-    return this.getDevice() === DeviceType.WindowsDesktop;
+    return (
+      this.getDevice() === DeviceType.WindowsDesktop || this.getDevice() === DeviceType.MacOsDesktop
+    );
   }
 
   supportsDuo(): boolean {
