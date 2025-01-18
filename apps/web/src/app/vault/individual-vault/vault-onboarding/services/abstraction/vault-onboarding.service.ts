bdc021be2707c5d62a4d8f209904af1f3bc10e17
--global <>
2025-01-17 19:10:37 -0500

    fixing tests

--- apps/web/src/app/vault/individual-vault/vault-onboarding/services/abstraction/vault-onboarding.service.ts
+++ apps/web/src/app/vault/individual-vault/vault-onboarding/services/abstraction/vault-onboarding.service.ts
@@ -8,5 +8,5 @@ import { VaultOnboardingTasks } from "../vault-onboarding.service";
 
 export abstract class VaultOnboardingService {
   abstract setVaultOnboardingTasks(newState: VaultOnboardingTasks): Promise<void>;
-  abstract vaultOnboardingState$(userId: UserId): Observable<VaultOnboardingTasks>;
+  abstract vaultOnboardingState$(userId: UserId): Observable<VaultOnboardingTasks | null>;
 }
