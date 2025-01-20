49cd6e0d08b8418606b3c5d335236bfe4a176007
gbubemismith <gsmithwalter@gmail.com>
2025-01-20 12:34:51 -0500

    Refactored totp service to use sdk
    Fixed strict typescript issues

--- libs/common/src/vault/abstractions/totp.service.ts
+++ libs/common/src/vault/abstractions/totp.service.ts
@@ -1,6 +1,4 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
 export abstract class TotpService {
-  getCode: (key: string) => Promise<string>;
-  getTimeInterval: (key: string) => number;
+  abstract getCode(key: string | undefined): Promise<string | undefined>;
+  abstract getTimeInterval(key: string): number;
 }
