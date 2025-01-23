b9c160c1aa6bf54470c22d3c02f34b715d5d328b
Bernd Schoolmann <mail@quexten.com>
2025-01-24 00:11:40 +0100

    Fix comment

--- apps/desktop/src/platform/services/electron-platform-utils.service.ts
+++ apps/desktop/src/platform/services/electron-platform-utils.service.ts
@@ -77,7 +77,7 @@ export class ElectronPlatformUtilsService implements PlatformUtilsService {
     return (await this.getApplicationVersion()).split(/[+|-]/)[0].trim();
   }
 
-  // Linux and Mac are missing a pinentry, sandboxed linux needs further work to allow the devices
+  // Linux and Mac are missing ui to enter a pin, so this works for two-factor security keys, when always-uv is not active
   supportsWebAuthn(win: Window): boolean {
     return true;
   }
