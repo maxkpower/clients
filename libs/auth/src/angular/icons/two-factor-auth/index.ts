94f151779b7006888f22ed86d50250c28f73e7b7
Jared Snider <jsnider@bitwarden.com>
2025-01-27 18:52:50 -0500

    PM-8113 - Add 2fa icons (icons need tweaking still)

---
+++ libs/auth/src/angular/icons/two-factor-auth/index.ts
@@ -0,0 +1,4 @@
+export * from "./two-factor-auth-authenticator.icon";
+export * from "./two-factor-auth-email.icon";
+export * from "./two-factor-auth-webauthn.icon";
+export * from "./two-factor-auth-yubikey.icon";
