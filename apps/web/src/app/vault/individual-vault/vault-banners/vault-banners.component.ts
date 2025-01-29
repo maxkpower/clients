4ea8513eb868f59ab1cf55349265c42a127a8984
Alec Rippberger <alec@livefront.com>
2025-01-29 16:06:48 -0600

    Remove ts-strict-ignore

--- apps/web/src/app/vault/individual-vault/vault-banners/vault-banners.component.ts
+++ apps/web/src/app/vault/individual-vault/vault-banners/vault-banners.component.ts
@@ -1,5 +1,3 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
 import { Component, Input, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Router } from "@angular/router";
