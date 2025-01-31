18cb48979fa3a0378dc8eb3cffb7c4b3eae0b710
Bernd Schoolmann <mail@quexten.com>
2025-01-31 18:06:39 +0100

    Change from tripleequal to double equal

--- apps/desktop/src/platform/services/ssh-agent.service.ts
+++ apps/desktop/src/platform/services/ssh-agent.service.ts
@@ -156,7 +156,7 @@ export class SshAgentService implements OnDestroy {
               (cipher) =>
                 cipher.type === CipherType.SshKey &&
                 !cipher.isDeleted &&
-                cipher.organizationId === null,
+                cipher.organizationId == null,
             );
             const keys = sshCiphers.map((cipher) => {
               return {
@@ -258,7 +258,7 @@ export class SshAgentService implements OnDestroy {
             (cipher) =>
               cipher.type === CipherType.SshKey &&
               !cipher.isDeleted &&
-              cipher.organizationId === null,
+              cipher.organizationId == null,
           );
           const keys = sshCiphers.map((cipher) => {
             return {
