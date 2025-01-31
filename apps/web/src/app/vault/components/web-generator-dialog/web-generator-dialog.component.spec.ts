5c8b93f3b70e80fdf36f25b7b1057f712b883f75
jaasen-livefront <jaasen@livefront.com>
2025-01-31 12:25:33 -0800

    update specs

--- apps/web/src/app/vault/components/web-generator-dialog/web-generator-dialog.component.spec.ts
+++ apps/web/src/app/vault/components/web-generator-dialog/web-generator-dialog.component.spec.ts
@@ -1,19 +1,19 @@
-// FIXME: Update this file to be type safe and remove this and next line
-// @ts-strict-ignore
-import { DialogRef, DIALOG_DATA } from "@angular/cdk/dialog";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { mock, MockProxy } from "jest-mock-extended";
 
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+import { AlgorithmInfo } from "@bitwarden/generator-core";
 import { CipherFormGeneratorComponent } from "@bitwarden/vault";
 
 import {
   WebVaultGeneratorDialogAction,
   WebVaultGeneratorDialogComponent,
-  WebVaultGeneratorDialogParams,
+  WebVaultGeneratorDialogResult,
 } from "./web-generator-dialog.component";
 
 @Component({
@@ -23,41 +23,27 @@ import {
 })
 class MockCipherFormGenerator {
   @Input() type: "password" | "username";
+  @Input() algorithmSelected: (selected: AlgorithmInfo) => void;
   @Output() valueGenerated = new EventEmitter<string>();
 }
 
 describe("WebVaultGeneratorDialogComponent", () => {
   let component: WebVaultGeneratorDialogComponent;
   let fixture: ComponentFixture<WebVaultGeneratorDialogComponent>;
-
-  let dialogRef: MockProxy<DialogRef<any>>;
+  let dialogRef: MockProxy<DialogRef<WebVaultGeneratorDialogResult>>;
   let mockI18nService: MockProxy<I18nService>;
 
   beforeEach(async () => {
-    dialogRef = mock<DialogRef<any>>();
+    dialogRef = mock<DialogRef<WebVaultGeneratorDialogResult>>();
     mockI18nService = mock<I18nService>();
-
-    const mockDialogData: WebVaultGeneratorDialogParams = { type: "password" };
 
     await TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, WebVaultGeneratorDialogComponent],
       providers: [
-        {
-          provide: DialogRef,
-          useValue: dialogRef,
-        },
-        {
-          provide: DIALOG_DATA,
-          useValue: mockDialogData,
-        },
-        {
-          provide: I18nService,
-          useValue: mockI18nService,
-        },
-        {
-          provide: PlatformUtilsService,
-          useValue: mock<PlatformUtilsService>(),
-        },
+        { provide: DialogRef, useValue: dialogRef },
+        { provide: DIALOG_DATA, useValue: { type: "password" } },
+        { provide: I18nService, useValue: mockI18nService },
+        { provide: PlatformUtilsService, useValue: mock<PlatformUtilsService>() },
       ],
     })
       .overrideComponent(WebVaultGeneratorDialogComponent, {
@@ -71,38 +57,50 @@ describe("WebVaultGeneratorDialogComponent", () => {
     fixture.detectChanges();
   });
 
-  it("initializes without errors", () => {
-    fixture.detectChanges();
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
-  it("closes the dialog with 'canceled' result when close is called", () => {
-    const closeSpy = jest.spyOn(dialogRef, "close");
+  it("should enable button when value and algorithm are selected", () => {
+    // Get the generator component
+    const generator = fixture.debugElement.query(
+      By.css("vault-cipher-form-generator"),
+    ).componentInstance;
 
-    (component as any).close();
+    // Simulate algorithm selection and value generation
+    generator.algorithmSelected({ useGeneratedValue: "Use Password" } as any);
+    generator.valueGenerated.emit("test-password");
+    fixture.detectChanges();
 
-    expect(closeSpy).toHaveBeenCalledWith({
+    // Check button state
+    const button = fixture.debugElement.query(
+      By.css("[data-testid='select-button']"),
+    ).nativeElement;
+    expect(button.disabled).toBe(false);
+  });
+
+  it("should close with selected value when confirmed", () => {
+    // Set up test data through component methods
+    const generator = fixture.debugElement.query(
+      By.css("vault-cipher-form-generator"),
+    ).componentInstance;
+    generator.algorithmSelected({ useGeneratedValue: "Use Password" } as any);
+    generator.valueGenerated.emit("test-password");
+    fixture.detectChanges();
+
+    // Click select button
+    fixture.debugElement.query(By.css("[data-testid='select-button']")).nativeElement.click();
+
+    expect(dialogRef.close).toHaveBeenCalledWith({
+      action: WebVaultGeneratorDialogAction.Selected,
+      generatedValue: "test-password",
+    });
+  });
+
+  it("should close with canceled action when dismissed", () => {
+    component["close"]();
+    expect(dialogRef.close).toHaveBeenCalledWith({
       action: WebVaultGeneratorDialogAction.Canceled,
     });
-  });
-
-  it("closes the dialog with 'selected' result when selectValue is called", () => {
-    const closeSpy = jest.spyOn(dialogRef, "close");
-    const generatedValue = "generated-value";
-    component.onValueGenerated(generatedValue);
-
-    (component as any).selectValue();
-
-    expect(closeSpy).toHaveBeenCalledWith({
-      action: WebVaultGeneratorDialogAction.Selected,
-      generatedValue: generatedValue,
-    });
-  });
-
-  it("updates generatedValue when onValueGenerated is called", () => {
-    const generatedValue = "new-generated-value";
-    component.onValueGenerated(generatedValue);
-
-    expect((component as any).generatedValue).toBe(generatedValue);
   });
 });
