52d4521de273b86e666e6c1a885d6bd78c6e7aa2
✨ Audrey ✨ <audrey@audreyality.com>
2025-02-03 17:10:50 -0500

    document pin

--- libs/common/src/tools/rx.ts
+++ libs/common/src/tools/rx.ts
@@ -215,6 +215,14 @@ export function on<T>(watch$: Observable<any>) {
   );
 }
 
+/** Create an observable that emits the first value from the source and
+ *  throws if the observable emits another value.
+ *  @param options.name names the pin to make discovering failing observables easier
+ *  @param options.distinct compares two emissions with each other to determine whether
+ *   the second emission is a duplicate. When this is specified, duplicates are ignored.
+ *   When this isn't specified, any emission after the first causes the pin to throw
+ *   an error.
+ */
 export function pin<T>(options?: {
   name?: () => string;
   distinct?: (previous: T, current: T) => boolean;
