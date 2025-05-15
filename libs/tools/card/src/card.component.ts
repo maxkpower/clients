e22728b5c4bef2b84689229a7d9e9b30900960d8
Maximilian Power <post@maximilianpower.com>
2025-05-15 02:16:55 +0200

    Change background color from white to background-alt to match design specs

--- libs/tools/card/src/card.component.ts
+++ libs/tools/card/src/card.component.ts
@@ -13,7 +13,7 @@ import { TypographyModule } from "@bitwarden/components";
   imports: [CommonModule, TypographyModule, JslibModule],
   host: {
     class:
-      "tw-box-border tw-bg-background tw-block tw-text-main tw-border-solid tw-border tw-border-secondary-300 tw-border [&:not(bit-layout_*)]:tw-rounded-lg tw-rounded-lg tw-p-6",
+      "tw-box-border tw-bg-background-alt tw-block tw-text-main tw-border-solid tw-border tw-border-secondary-300 tw-border [&:not(bit-layout_*)]:tw-rounded-lg tw-rounded-lg tw-p-6",
   },
 })
 export class CardComponent {
